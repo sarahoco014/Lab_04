3c4b7588006e64600fd09d78752ede5d8bf2a2de
sarahoco014 <soconnor2@outlook.com>
2023-07-11 18:41:50 +0100

    adds computer answers

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,2 +1,39 @@
 public class Computer {
+
+    int storage;
+    String printerModel;
+
+    String message;
+
+    public Computer(int storage, String printerModel) {
+        this.storage = storage;
+        this.printerModel = printerModel;
+    }
+
+    int getStorage() {
+        return storage;
+    }
+
+    void addStorage(int newStorage) {
+        this.storage += newStorage;
+    }
+
+    void setPrinterModel(String printerModel) {
+        this.printerModel = printerModel;
+    }
+
+    String getPrinterModel() {
+        return printerModel;
+    }
+
+    // put getters and setters together
+
+    void printMessage(String message) {
+        System.out.println("Printing...");
+        System.out.println("The printed message is: " + message);
+    }
 }
+
+
+//Computer.getStorage();
+//Computer.setStorage(5);
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -29,27 +29,29 @@ public class Runner {
         System.out.println("Running computer tasks...");
 
         // 1. Create a new computer object
-//        Computer computer = <INSTANTIATE A NEW COMPUTER OBJECT HERE>
+        Computer computer = new Computer(3, "Samsung");
 
         // 2. Use a getter to find out how much storage the computer has
-//        int currentStorage = <CALL YOUR METHOD HERE>;
-//        System.out.println("Computer has " + currentStorage + "GB storage");
+        int currentStorage = computer.getStorage();
+        System.out.println("Computer has " + currentStorage + "GB storage");
 
         // 3. Add more storage to the computer
-//        <CALL YOUR METHOD HERE>;
+        computer.addStorage(2);
+
         // We use the getter again to check that the amount of storage has been updated
-//        int newStorage = computer.getStorage();
-//        System.out.println("Computer now has " + newStorage + "GB storage");
+        int newStorage = computer.getStorage();
+        System.out.println("Computer now has " + newStorage + "GB storage");
 
         // 4. Use a setter to update the computer's printer property
-//        <CALL YOUR METHOD HERE>
+        computer.setPrinterModel("HP");
+
         // Write a getter and use it below to check that the setter worked
-//        String currentPrinter = <GETTER>;
-//        System.out.println("Computer currently has a " + currentPrinter + " printer connected.");
+        String currentPrinter = computer.getPrinterModel();
+        System.out.println("Computer currently has a " + currentPrinter + " printer connected.");
 
         // 5. Write a method to print a message.
         // To test this works *without* a printer too you can comment out the code for question 4
-//        computer.printMessage("Hello World!");
+        computer.printMessage("Hello World!");
     }
 
     public static void shoppingTrolleyDemo(){
