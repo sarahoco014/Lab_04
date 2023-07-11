3c4c15b66cb3e2f8186bc72413e9915b0ef56471
sarahoco014 <soconnor2@outlook.com>
2023-07-11 17:58:22 +0100

    adds rectangle tasks

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,20 @@
 public class Rectangle {
+
+    int length;
+    int width;
+    int area;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+        this.area = length * width;
+    }
+
+    int calculateArea() {
+        return length * width;
+    }
+
+    boolean isSquare() {
+        return length == width;
+    }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,17 +11,17 @@ public class Runner {
         System.out.println("Running rectangle tasks...");
 
         // 1. Create a new rectangle object
-//        Rectangle rectangle = <INSTANTIATE A NEW RECTANGLE OBJECT HERE>
+        Rectangle rectangle = new Rectangle(2,2);
 
         //testing commits
 
         // 2. Calculate the rectangle's area and store it in a variable
-//        int area = <CALL YOUR METHOD HERE>;
-//        System.out.println("The rectangle has an area of " + area);
+        int area = rectangle.area;
+        System.out.println("The rectangle has an area of " + area);
 
         // 3. Check if your rectangle is actually a square
-//        boolean isRectangle = <CALL YOUR METHOD HERE>
-//        System.out.println("Object is a square (true/false): " + isRectangle);
+        boolean isRectangle = rectangle.isSquare();
+        System.out.println("Object is a square (true/false): " + isRectangle);
     }
 
     public static void computerDemo(){
